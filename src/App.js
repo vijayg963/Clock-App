03bedcafa7390b80b57a41e80f7a97254024df9d
Vijay Gupta <vijayg963@gmail.com>
2022-06-28 14:51:35 +0530

    StopWatch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Countdown from './components/Countdown';
+import Stopwatch from './components/Stopwatch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      countDown: false,
+      stopWatch: false,
+    };
+  }
+  handleClose = () => {
+    this.setState({
+      countDown: false,
+    });
+  };
+  handleStopWatch = () => {
+    this.setState({
+      stopWatch: false,
+    });
+  };
+  render() {
+    return (
+      <div className='App'>
+        <h1>✌ Timer App ⏲ 🚀 Timers 🚀</h1>
+        <div className='container'>
+          <section className='box'>
+            <button
+              className={this.state.countDown ? 'hidden' : ''}
+              onClick={() =>
+                this.setState({
+                  countDown: true,
+                })
+              }
+            >
+              Countdown
+            </button>
+            <div className={this.state.countDown ? '' : 'hidden'}>
+              <Countdown handleClose={this.handleClose} />
+            </div>
+          </section>
+          <section className='box'>
+            <button
+              className={this.setState.stopWatch ? 'hidden' : ''}
+              onClick={() =>
+                this.setState({
+                  stopWatch: true,
+                })
+              }
+            >
+              Stopwatch
+            </button>
+            <div className={this.state.stopWatch ? '' : 'hidden'}>
+              <Stopwatch handleStopWatch={this.handleStopWatch} />
+            </div>
+          </section>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
